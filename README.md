084c67bfd130d011440c79a7aec8d41393c5554d
allenliu <liusheng@fooyo.sg>
2017-09-19 16:12:27 +0800

    init frandroid sdk

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # FRAndroidSDK
 Fooyo photo collage sdk
+
+## include
+ - add following script into your root build.gradle
+ ```
+ allprojects {
+     repositories {
+         jcenter()
+         maven {
+             url "https://jitpack.io"
+ 
+         }
+         maven {
+             url "https://maven.google.com"
+         }
+         maven {
+             url 'https://dl.bintray.com/fooyo/FRAndroidSdk/'
+         }
+     }
+ }
+ ```
+ - because of sdk using databinding lib,so you should add databinding into your module build.gradle
+ 
+ ```
+ android {
+     ...
+     
+     dataBinding {
+         enabled = true
+     }
+ }
+ ```
+ - add dependency into gradle
+ ```
+     compile 'sg.fooyo:FRAndroidSdk:1.0.1'
+```
+
+## Create photo collage
+
+- `startTime` ,this param represents the time that photo collage start to search.
+```
+FooyoFRAndroidSDK.createPhotoCollage(Context context, long startTime);
+```
---
+++ app/src/main/java/com/allenliu/frandroiddemo/MainActivity.java
@@ -0,0 +1,80 @@
+package com.allenliu.frandroiddemo;
+
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.content.DialogInterface;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.DatePicker;
+
+import com.blankj.utilcode.util.LogUtils;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+import sg.fooyo.frandroidsdk.sdk.FooyoFRAndroidSDK;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getDate();
+    }
+
+    DatePickerDialog.OnDateSetListener listener;
+    private int year, month, day;
+    private Calendar cal;
+    long startTime = 1505516827;
+
+    public void onClick(View v) {
+
+        final DatePickerDialog datePickerDialog = new DatePickerDialog(MainActivity.this, 0, listener, year, month, day);//后边三个参数为显示dialog时默认的日期，月份从0开始，0-11对应1-12个月
+        datePickerDialog.setButton("ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+
+                Date date;
+                SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+                int y = datePickerDialog.getDatePicker().getYear();
+                int m = datePickerDialog.getDatePicker().getMonth();
+                int d = datePickerDialog.getDatePicker().getDayOfMonth();
+                try {
+                    Calendar calendar = Calendar.getInstance();
+                    calendar.set(y, m, d);
+                    startTime = calendar.getTime().getTime();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                LogUtils.e("startTime:" + startTime + "\nstartTime:" + System.currentTimeMillis());
+                FooyoFRAndroidSDK.createPhotoCollage(MainActivity.this, startTime);
+            }
+        });
+        datePickerDialog.show();
+
+
+    }
+
+    //获取当前日期
+    private void getDate() {
+        cal = Calendar.getInstance();
+        year = cal.get(Calendar.YEAR);       //获取年月日时分秒
+        month = cal.get(Calendar.MONTH);   //获取到的月份是从0开始计数
+        day = cal.get(Calendar.DAY_OF_MONTH);
+        listener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker arg0, int year, int month, int day) {
+                //将选择的日期显示到TextView中,因为之前获取month直接使用，所以不需要+1，这个地方需要显示，所以+1
+
+
+            }
+        };
+    }
+}
